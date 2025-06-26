6e3f11534049d1b7511399410bfffe8f3c96ad3d
Ashraf Salih <assa00001@stud.uni-saarland.de>
2025-06-26 13:55:13 +0200

    t6-t8

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from socialnetwork.views.html import timeline
-from socialnetwork.views.html import follow
-from socialnetwork.views.html import unfollow
+from socialnetwork.views.html import (
+    timeline,
+    follow,
+    unfollow,
+    bullshitters,
+    toggle_community_mode,
+    join_community,
+    leave_community,
+    similar_users
+)
 from socialnetwork.views.rest import PostsListApiView
 
 app_name = "socialnetwork"
@@ -12,4 +19,9 @@ urlpatterns = [
     path("html/timeline", timeline, name="timeline"),
     path("api/follow", follow, name="follow"),
     path("api/unfollow", unfollow, name="unfollow"),
+    path("html/bullshitters", bullshitters, name="bullshitters"),
+    path("api/toggle_community_mode", toggle_community_mode, name="toggle_community_mode"),
+    path("api/join_community", join_community, name="join_community"),
+    path("api/leave_community", leave_community, name="leave_community"),
+    path("html/similar_users", similar_users, name="similar_users"),
 ]
